ab06b4a96853135597bf33d8cc9e8ccb6ab3afc4
Suelt <2552925383@qq.com>
2023-02-23 18:42:38 -0800

    libp2ptool construction

--- p2p/libp2pconn.go
+++ p2p/libp2pconn.go
@@ -13,7 +13,11 @@ import (
 	"github.com/hashicorp/go-msgpack/codec"
 	"github.com/libp2p/go-libp2p/core/host"
 	"github.com/libp2p/go-libp2p/core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
+	"github.com/libp2p/go-libp2p/core/peerstore"
 	"github.com/libp2p/go-libp2p/core/protocol"
+
+	"github.com/multiformats/go-multiaddr"
 )
 
 type MsgWithSig struct {
@@ -29,10 +33,11 @@ type NetworkDealer struct {
 	shutdownCh   chan struct{}
 	shutdownLock sync.Mutex
 
-	timeout           time.Duration
-	ctx               context.Context
-	ctxCancel         context.CancelFunc
-	ctxLock           sync.RWMutex
+	timeout time.Duration
+	// ctx               context.Context
+	// ctxCancel         context.CancelFunc
+	// ctxLock           sync.RWMutex
+
 	reflectedTypesMap map[uint8]reflect.Type
 }
 
@@ -92,10 +97,70 @@ func (n *NetworkDealer) HandleConn(r *bufio.Reader) error {
 	return nil
 }
 
-func (n *NetworkDealer) Connect(port int, addr string) {
-
+func (n *NetworkDealer) Connect(port int, addr string, pubKey string) (*bufio.Writer, error) {
+	return n.ConnectWithMultiaddr(PackMultiaddr(port, addr, pubKey))
 }
 
-func (n *NetworkDealer) ConnectWithMultiaddr(multi string) {
+func (n *NetworkDealer) ConnectWithMultiaddr(multi string) (*bufio.Writer, error) {
 
+	log.Println("Connecting to ", multi)
+	maddr, err := multiaddr.NewMultiaddr(multi)
+	if err != nil {
+		return nil, err
+	}
+
+	// Extract the peer ID from the multiaddr.
+	info, err := peer.AddrInfoFromP2pAddr(maddr)
+	if err != nil {
+		return nil, err
+	}
+	n.h.Peerstore().AddAddr(info.ID, maddr, peerstore.PermanentAddrTTL)
+	s, err := n.h.NewStream(context.Background(), info.ID, protocol.ID("PlainDAG"))
+	if err != nil {
+		return nil, err
+	}
+	return bufio.NewWriter(s), nil
+}
+
+func PackMultiaddr(port int, addr string, pubKey string) string {
+	return fmt.Sprintf("ip4/%s/tcp/%v/p2p/%s", addr, port, pubKey)
+}
+
+func (n *NetworkDealer) SendMsg(rpcType uint8, msg interface{}, sig []byte, dest string) error {
+
+	n.shutdownLock.Lock()
+	if n.shutdown {
+		n.shutdownLock.Unlock()
+		return errors.New("shut down")
+	}
+	n.shutdownLock.Unlock()
+
+	c, ok := n.connPool[dest]
+	if !ok {
+		w, err := n.ConnectWithMultiaddr(dest)
+		if err != nil {
+			return err
+		}
+
+		c = &conn{
+			dest:   dest,
+			w:      w,
+			encode: codec.NewEncoder(w, &codec.MsgpackHandle{}),
+		}
+		n.connPool[dest] = c
+	}
+
+	if err := c.w.WriteByte(rpcType); err != nil {
+		return err
+	}
+	if err := c.encode.Encode(msg); err != nil {
+		return err
+	}
+	if err := c.encode.Encode(sig); err != nil {
+		return err
+	}
+	if err := c.w.Flush(); err != nil {
+		return err
+	}
+	return nil
 }
